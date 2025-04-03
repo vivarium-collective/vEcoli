7744c49d1daae45400deb95c77becd897ed4916c
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2025-04-02 20:17:43 -0400

    updated gitignore and added type funcs to ecoli init

--- ecoli/__init__.py
+++ ecoli/__init__.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import sys
 
 # Improve performance and reproducibility
@@ -153,6 +154,18 @@ def register_types(core: ProcessTypes, types_dir: str) -> None:
         except AssertionError as e:
             logger.error(str(e))
             continue
+
+
+def check_bytes(schema, state, core=None):
+    return isinstance(state, bytes)
+
+
+def apply_bytes(schema, current, update: bytes, core=None):
+    return current + update
+
+
+def deserialize_bytes(schema, encoded, core=None):
+    return pickle.loads(encoded)
 
 
 # project core singleton
