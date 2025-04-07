c66f6c86ef86a1771e5ed9bb917f1495f2f0e200
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2025-04-07 12:17:48 -0400

    added new copied types to emulate bulk and numpy dtypes

--- ecoli/shared/dtypes.py
+++ ecoli/shared/dtypes.py
@@ -1,13 +1,29 @@
+from typing import Any
+
 import numpy as np
 from numpy.typing import NDArray
 
 
-BULK_DTYPE: np.dtype = np.dtype([
+bulk_dtype: np.dtype = np.dtype([
     ("id", "<U100"),
     ("count", "<f8")
 ])
 
+active_replisomes_dtype: np.dtype = np.dtype([
+    ("id", "<U100"),
+    ("_entryState", np.ndarray | Any)
+])
 
-def format_bulk_state(state: dict) -> NDArray[BULK_DTYPE]:
+
+def format_state(state: dict, key: str, dtype: np.dtype) -> NDArray[bulk_dtype]:
     """"""
-    return np.array(state["bulk"], dtype=BULK_DTYPE)
+    return np.array(state[key], dtype=dtype)
+
+
+def format_bulk_state(state: dict) -> NDArray[bulk_dtype]:
+    """"""
+    return format_state(state, "bulk", bulk_dtype)
+
+
+def format_active_replisomes_state(state: dict) -> NDArray[active_replisomes_dtype]:
+    return format_state(state, "active_replisomes", active_replisomes_dtype)
