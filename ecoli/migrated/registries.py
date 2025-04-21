54e2ae48015d8b2cf9af8f4782488b63e00deee1
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2025-04-21 12:41:59 -0400

    made new migration utils and rm workflows

--- ecoli/migrated/registries.py
+++ ecoli/migrated/registries.py
@@ -1,39 +1,80 @@
 # TODO: provide clean registration here!
 
 from dataclasses import dataclass
+import datetime
 from typing import Any
 
-from process_bigraph import ProcessTypes
+from process_bigraph import ProcessTypes, Process
+from vivarium.core.registry import Registry as VivRegistry
+from bigraph_schema import Registry as BgsRegistry
 
 
-#: Maps process names to topology
-# topology_registry = Registry()
+
+class TopologyRegistry(VivRegistry):
+    """Maps process names to topology"""
+    pass
+
+
+class ProcessRegistry(BgsRegistry):
+    pass
 
 
 @dataclass
-class Get:
-    core: ProcessTypes | Any
+class DataView:
+    _processes: ProcessRegistry | BgsRegistry
+    _topology: TopologyRegistry | None = None
 
     @property
     def processes(self):
-        return list(self.core.process_registry.registry.keys())
+        return list(self._processes.registry.keys())
     
     @property
     def types(self):
-        return(self.core.process_registry.registry.keys())
+        return(self._processes.registry.keys())
     
+    @property
+    def topology(self):
+        return self._topology.registry if self._topology else {}
+
 
 class Core(ProcessTypes):
-    def __init__(self, *args, **kwargs):
+    def __init__(
+            self, 
+            topology_registry: TopologyRegistry | None = None, 
+            *args, 
+            **kwargs):
         super().__init__(*args, **kwargs)
+        self.last_update: str = self.timestamp()
+        self._topology = topology_registry or TopologyRegistry()
+        self._view = DataView(self.processes, self.topology)
+    
+    def timestamp(self):
+        return str(datetime.datetime.now())
     
     @property
-    def get(self):
-        return Get(core=self)
-    
-    @property
-    def processes(self):
+    def processes(self) -> BgsRegistry:
         return self.process_registry
     
+    @property
+    def topology(self) -> TopologyRegistry:
+        return self._topology
     
-ecoli_core = Core()
+    @property
+    def view(self) -> DataView:
+        """
+        Allows for dot notation throughout a get call, ie: Core().view.processes <- gets process names that are registered.
+        Possibly view callables are: processes, types, and topology
+        """
+        return self._view
+    
+    @view.setter
+    def view(self, v):
+        raise AssertionError("You cannot set the view.")
+    
+    
+    
+    
+    
+topology_registry = TopologyRegistry()
+ecoli_core = Core(topology_registry=topology_registry)
+
