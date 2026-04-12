5c7e35e088c5103b34882b7b4922b309e41c0be2
Ryan Spangler <ryan.spangler@gmail.com>
2026-04-12 03:13:31 -0700

    schema for rna counts

--- ecoli/processes/chromosome_structure.py
+++ ecoli/processes/chromosome_structure.py
@@ -111,7 +111,7 @@ class ChromosomeStructure(Step):
         'n_mature_rnas': 'integer{0}',
         'mature_rna_ids': 'array[string]',
         'mature_rna_end_positions': 'array[float]',
-        'mature_rna_nt_counts': {'_type': 'node', '_default': []},
+        'mature_rna_nt_counts': 'array[integer[64]]',
         'unprocessed_rna_index_mapping': 'map[integer,integer]',
         'time_step': 'float{1.0}',
     }
